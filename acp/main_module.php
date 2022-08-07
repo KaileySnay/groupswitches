236f350475f2a8f844d1d81b3029aafe6c6a871a
kaileysnay <kailey.truscott@layer-3.org>
2022-08-06 21:27:03 -0400

    Use dependency injection through $phpbb_container

--- acp/main_module.php
+++ acp/main_module.php
@@ -15,46 +15,67 @@ namespace kaileysnay\groupswitches\acp;
  */
 class main_module
 {
+	/** @var \phpbb\db\driver\driver_interface */
+	protected $db;
+
+	/** @var \phpbb\group\helper $group_helper */
+	protected $group_helper;
+
+	/** @var \phpbb\language\language */
+	protected $language;
+
+	/** @var \phpbb\template\template */
+	protected $template;
+
+	/** @var string */
 	public $page_title;
+
+	/** @var string */
 	public $tpl_name;
+
+	/** @var string */
 	public $u_action;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		global $phpbb_container;
+
+		$this->db = $phpbb_container->get('dbal.conn');
+		$this->group_helper = $phpbb_container->get('group_helper');
+		$this->language = $phpbb_container->get('language');
+		$this->template = $phpbb_container->get('template');
+
+		$this->tables['groups'] = $phpbb_container->getParameter('tables.groups');
+	}
 
 	/**
 	 * Main ACP module
 	 */
 	public function main($id, $mode)
 	{
-		global $db, $template;
-		global $phpbb_container;
-
-		/** @var \phpbb\language\language $language */
-		$language = $phpbb_container->get('language');
-
-		/** @var \phpbb\group\helper $group_helper */
-		$group_helper = $phpbb_container->get('group_helper');
-
 		// Load a template from adm/style for our ACP page
 		$this->tpl_name = 'acp_groupswitches_body';
 
 		// Set the page title for our ACP page
-		$this->page_title = $language->lang('GROUP_SWITCHES');
+		$this->page_title = $this->language->lang('GROUP_SWITCHES');
 
 		// Grab all the groups
-		$groups_table = $phpbb_container->getParameter('tables.groups');
-
 		$sql = 'SELECT group_name, group_id
-			FROM ' . $groups_table . '
+			FROM ' . $this->tables['groups'] . '
 			ORDER BY group_name';
-		$result = $db->sql_query($sql);
-		while ($row = $db->sql_fetchrow($result))
+		$result = $this->db->sql_query($sql);
+		while ($row = $this->db->sql_fetchrow($result))
 		{
-			$group_name = $group_helper->get_name($row['group_name']);
+			$group_name = $this->group_helper->get_name($row['group_name']);
 
-			$template->assign_block_vars('groups', [
+			$this->template->assign_block_vars('groups', [
 				'GROUP_NAME'	=> $group_name,
 				'GROUP_ID'		=> $row['group_id'],
 			]);
 		}
-		$db->sql_freeresult($result);
+		$this->db->sql_freeresult($result);
 	}
 }
